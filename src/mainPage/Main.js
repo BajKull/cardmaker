ad78424dcaa5919e7be6570ba85deb5e4a849f4c
Dawid Banachowski <bajkull2@gmail.com>
2020-09-20 22:40:47 +0200

    landing page redesign

--- src/mainPage/Main.js
+++ src/mainPage/Main.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Bg from "./bg.png";
+import { ReactComponent as Blob } from "./blob.svg";
 import { Element, Link } from "react-scroll";
 
 export default function main() {
   return (
     <Element name="mainPage" className="mainPage">
-      <img className="mainBg" src={Bg} alt="" />
-      <div className="mainBgFilter"></div>
-      <h1>Create your own card</h1>
-      <h3>Easy, fast and pretty</h3>
-      <Link to="choice" smooth>
-        <button className="mainBtn">Get started</button>
-      </Link>
+      <div className="mainBg"></div>
+      <div className="mainContent">
+        <div className="mainText">
+          <h1>Create your own card</h1>
+          <h2>Easy, fast and pretty</h2>
+          <p>
+            Design your own card from scratch or choose a template and share it
+            for free!
+          </p>
+          <Link to="choice" smooth>
+            <button className="mainBtn">Get started</button>
+          </Link>
+        </div>
+        <div className="mainImgContainer">
+          <Blob className="mainBlob" />
+          <img className="mainImg" src={Bg} alt="" />
+        </div>
+      </div>
     </Element>
   );
 }
