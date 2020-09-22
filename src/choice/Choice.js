7eba17f984c07848092a713cca7feeedb4fd0a1a
Dawid Banachowski <bajkull2@gmail.com>
2020-09-22 17:00:58 +0200

    choice page redesign

--- src/choice/Choice.js
+++ src/choice/Choice.js
@@ -1,13 +1,45 @@
 import React from "react";
 import Cards from "./Cards";
 import { Element } from "react-scroll";
+import { Link } from "react-router-dom";
+import { ReactComponent as Blob } from "./blob.svg";
+import Screen1 from "./screen1.png";
 
 export default function Choice() {
   return (
     <Element name="choice" className="choice">
+      <Blob className="choiceBlob" />
       <div className="choiceBg"></div>
-      <h1>Choose your design</h1>
-      <Cards />
+      <div className="choice1">
+        <div className="choiceText">
+          <h1>Browse free and available patterns</h1>
+          <h2>
+            There are many various cards which you can browse and choose
+            whichever one pleases and suits your needs. Every card is free for
+            everyone! If we think your card is standing out we may add it to our
+            global collection!
+          </h2>
+          <Link to="/cards">
+            <button className="choiceBtn">Browse cards</button>
+          </Link>
+        </div>
+        <img src={Screen1} alt="" className="choiceImg" />
+      </div>
+      <div className="choice2">
+        <div className="choiceText">
+          <h1>Create your own card from scratch</h1>
+          <h2>
+            If you do not want to use a pattern you can design your card by
+            yourself and show your creativity. Sky is the limit!
+          </h2>
+          <Link to="/create">
+            <button className="choiceBtn">Create card</button>
+          </Link>
+        </div>
+        <img src={Screen1} alt="" className="choiceImg" />
+      </div>
+
+      {/* <Cards /> */}
     </Element>
   );
 }
