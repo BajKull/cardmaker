92ceecd56278f4810bf8d853a042d658fcf8fce0
Dawid Banachowski <bajkull2@gmail.com>
2020-09-24 20:40:46 +0200

    navbar, login, register responsiveness

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,43 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { ChangeLoginScreen } from "../redux/actions/ChangeLoginScreen";
 import { ReactComponent as LogoIcon } from "./logo4.svg";
+import { ReactComponent as Burger } from "./burger.svg";
+import { CSSTransition } from "react-transition-group";
+
+function Links({ name, user, handleOnClick, handleMenuClick, showMenu }) {
+  return (
+    <div className={name}>
+      {showMenu && (
+        <div className="navbarBg" onClick={() => handleMenuClick()}></div>
+      )}
+      <div className="navbarContent">
+        <ul className="navbarLinks">
+          <li>
+            <Link to="/">
+              <LogoIcon className="logo" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/cards">Browse</Link>
+          </li>
+        </ul>
+        {!user && (
+          <ul className="navbarLogin">
+            <li onClick={() => handleOnClick("signin")}>Sign in</li>
+            <li onClick={() => handleOnClick("signup")}>Sign up</li>
+          </ul>
+        )}
+        {user && (
+          <ul className="navbarLogin">
+            <li>{user.displayName}</li>
+          </ul>
+        )}
+        <Link to="/create">
+          <button className="navbarBtn" onClick={() => handleMenuClick()}>
+            Get started
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default function Navbar() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.loginStatus);
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleOnClick = (screen) => {
     dispatch(ChangeLoginScreen(screen));
   };
+
+  const handleMenuClick = () => {
+    if (showMenu) document.body.style.overflow = "auto";
+    else document.body.style.overflow = "hidden";
+
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="navbar">
-      <ul className="navbarLinks">
-        <li>
-          <Link to="/">
-            <LogoIcon className="logo" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/cards">Browse</Link>
-        </li>
-      </ul>
-      {!user && (
-        <ul className="navbarLogin">
-          <li onClick={() => handleOnClick("signin")}>Sign in</li>
-          <li onClick={() => handleOnClick("signup")}>Sign up</li>
-        </ul>
-      )}
-      {user && (
-        <ul className="navbarLogin">
-          <li>{user.displayName}</li>
-        </ul>
-      )}
+      <Links
+        user={user}
+        handleOnClick={handleOnClick}
+        name={"navbarContainer"}
+      />
+      <Burger className="navbarBurger" onClick={handleMenuClick} />
+      <CSSTransition
+        in={showMenu}
+        timeout={500}
+        unmountOnExit
+        classNames="page-from-left"
+      >
+        <Links
+          user={user}
+          handleOnClick={handleOnClick}
+          name={"navbarContainer2"}
+          handleMenuClick={handleMenuClick}
+          showMenu={showMenu}
+        />
+      </CSSTransition>
     </div>
   );
 }
