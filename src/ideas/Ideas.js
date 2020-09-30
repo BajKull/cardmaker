6fad0e149ba2d8d14a5336da9cf446867288b12b
Dawid Banachowski <bajkull2@gmail.com>
2020-09-30 19:10:23 +0200

    popular cards

--- src/ideas/Ideas.js
+++ src/ideas/Ideas.js
@@ -1,15 +1,38 @@
-import React from "react";
-import Idea from "./Idea";
-import IdeasImgs from "./IdeasData";
+import React, { useEffect, useState } from "react";
 import { Element } from "react-scroll";
+import { projectFirestore } from "../firebase/Config";
+import { Link } from "react-router-dom";
 
 export default function Ideas() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    projectFirestore
+      .collection("cards")
+      .where("category", "==", "unchecked")
+      .orderBy("views", "desc")
+      .limit(3)
+      .get()
+      .then((data) => {
+        const els = [];
+        data.forEach((doc) => {
+          const el = { ...doc.data(), id: doc.id };
+          els.push(el);
+        });
+        setCards(els);
+      });
+  }, []);
+
   return (
     <Element name="idea" className="ideas">
       <h1 className="ideasTitle">Popular</h1>
       <div className="ideasCards">
-        {IdeasImgs.images.map((idea, i) => (
-          <Idea image={idea} key={i} />
+        {cards.map((card) => (
+          <Link to={`/cards/${card.id}`} className="idea" key={card.id}>
+            <img className="ideaImg" src={card.thumbnail} alt="" />
+            <div className="ideaBg"></div>
+            <button className="ideaBtn">Try it</button>
+          </Link>
         ))}
       </div>
     </Element>
